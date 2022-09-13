11827c14765b3aea6a1e1b26bda4341f8e312f22
Max Herrera <harvey.herrera@lumation.co>
2022-09-13 00:25:39 -0600

    View subcomponents are created to create the cards and buttons for the onboarding screen.

--- fruits/App/fruitsApp.swift
+++ fruits/App/fruitsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct fruitsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OnboardingView()
         }
     }
 }
---
+++ fruits/View/StartButton.swift
@@ -0,0 +1,43 @@
+//
+//  StartButton.swift
+//  fruits
+//
+//  Created by Max Herrera on 9/12/22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Button(action: {
+             
+        }) {
+            HStack(spacing: 8) {
+                Text("Start")
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+            )
+        } //: Button
+        .accentColor(Color.white)
+        
+        
+    }
+}
+
+// MARK: - PREVIEW
+struct StartButton_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
