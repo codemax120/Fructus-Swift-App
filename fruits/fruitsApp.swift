05857d42cac6d81d20be1e2f91172a4864101d99
Max Herrera <harvey.herrera@lumation.co>
2022-09-12 20:29:42 -0600

    Initial Commit

---
+++ fruits/fruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitsApp.swift
+//  fruits
+//
+//  Created by Max Herrera on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct fruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
