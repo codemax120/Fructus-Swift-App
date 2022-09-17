570952f17ea09b085c444deba26eb7ec6921173b
Max Herrera <harvey.herrera@lumation.co>
2022-09-17 16:51:47 -0600

    Added fruit detail screen to show title, headline, subheadline, description, link information.

---
+++ fruits/App/FruitDetailView.swift
@@ -0,0 +1,68 @@
+//
+//  FruitDetailView.swift
+//  fruits
+//
+//  Created by Max Herrera on 9/17/22.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+
+    // MARK: - PROPERTIES
+    var fruit: Fruit
+    
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    // MARK: - HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        
+                        // MARK: - TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // MARK: - HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        // MARK: - NUTRIENTS
+                        
+                        // MARK: - SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // MARK: - DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        // MARK: - LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                        
+                    }//: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }//: VSTACK
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarHidden(true)
+            } //: SCROLL
+            .edgesIgnoringSafeArea(.top)
+        } //: NAVIGATION
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+    }
+}
