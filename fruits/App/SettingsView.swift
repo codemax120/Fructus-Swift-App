4179a2b111158e34038b3be529b6331432db96e3
Max Herrera <harvey.herrera@lumation.co>
2022-09-17 18:35:43 -0600

    Added section for restarting the application

--- fruits/App/SettingsView.swift
+++ fruits/App/SettingsView.swift
@@ -11,6 +11,7 @@ struct SettingsView: View {
     
     // MARK: - PROPERTIES
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     var body: some View {
         // MARK: - BODY
@@ -38,6 +39,24 @@ struct SettingsView: View {
                     }
                     // MARK: - SECTION 2
                     
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toogle the switch in this box. That way it restarts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            Text("Restart".uppercased())
+                        }
+                        
+                    }
                     
                     // MARK: - SECTION 3
                     GroupBox(
